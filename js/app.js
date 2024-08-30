9eb48f7cb006190a06622d12d19aaf88fb78e566
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-08-31 00:43:51 +0700

    add none list

--- js/app.js
+++ js/app.js
@@ -3,23 +3,38 @@ class App{
     menu_sel=0;
 
     onLoad(){
+        ce.extension_id="ekfllnbpcoejhfbelcngmlkhjjihlblj";
+        ce.setColor("#82b74b");
         app.show_main();
+        ce.get_list_app_other();
+        $("#footer-donation").html(ce.donation_html());
     }
 
     show_menu(){
-        $("#app_menu").html('');
-        
-        var btn_match=$('<a class="btn '+(app.menu_sel==0 ? "btn-dark" : "btn-light")+' btn-sm m-1" href="#"><i class="fas fa-futbol"></i> Matches</a>');
+        $("#app_menu").empty();
+        var btn_match=$('<li class="nav-item"><a class="nav-link '+(app.menu_sel==0 ? "active" : "")+'" href="#"><i class="fas fa-futbol"></i> Matches</a></li>');
         $(btn_match).click(function(){
             app.show_main();
         });
         $("#app_menu").append(btn_match);
 
-        var btn_scorer=$('<a class="btn '+(app.menu_sel==1 ? "btn-dark" : "btn-light")+' btn-sm m-1" href="#"><i class="fas fa-running"></i> Goal scorer</a>');
+        var btn_scorer=$('<li class="nav-item"><a class="nav-link '+(app.menu_sel==1 ? "active" : "")+'" href="#"><i class="fas fa-running"></i> Scorer</a></li>');
         $(btn_scorer).click(function(){
             app.show_competitions();
         });
         $("#app_menu").append(btn_scorer);
+
+        var btn_list=$('<li class="nav-item"><a class="nav-link '+(app.menu_sel==2 ? "active" : "")+'" href="#"><i class="fas fa-list"></i> Bookmark</a></li>');
+        $(btn_list).click(function(){
+            app.show_bookmarks();
+        });
+        $("#app_menu").append(btn_list);
+
+        var btn_rate=$('<li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-star"></i> Rate</a></li>');
+        $(btn_rate).click(function(){
+            ce.rate();
+        });
+        $("#app_menu").append(btn_rate);
     }
 
     show_main(){
@@ -31,6 +46,7 @@ class App{
             $.each(matches,function(index,m){
                 $('#body_main').append(app.box_matche_item(m));
             });
+            ce.top();
         },()=>{
             $('#body_main').html('<p>Error !</p>');
         });
@@ -43,7 +59,13 @@ class App{
         var awayTeam=data.awayTeam;
         var html='';
         html+='<div role="button" class="card bg-light mb-3">';
-        html+='<div class="card-header"><img src="'+area.flag+'" style="width:15px"/> '+area.name+'</div>';
+        html+='<div class="card-header">';
+            html+='<img src="'+area.flag+'" style="width:15px"/> '+area.name;
+            if(data["index_offline"]!=null)
+                html+='<button class="btn-del-bookmark btn btn-sm" style="float:right"><i class="fas fa-trash-alt"></i></button>';
+            else
+                html+='<button class="btn-add-bookmark btn btn-sm" style="float:right"><i class="fas fa-plus-square"></i></button>';
+        html+='</div>';
         html+='<div class="card-body text-center fs-6">';
             html+='<div class="row card-text" style="font-size:12px">';
                 html+='<div class="col-4">';
@@ -67,6 +89,24 @@ class App{
         $(emp_matche).click(function(){
             app.show_info_matche(data);
         });
+
+        if(data["index_offline"]!=null){
+            $(emp_matche).find(".btn-del-bookmark").click(function(){
+                var index_item=data["index_offline"];
+                app.delete_item_bookmark(index_item,()=>{
+                    ce.msg("Delete item success!","Delete","success");
+                    app.show_bookmarks();
+                });
+                return false;
+            });
+        }else{
+            $(emp_matche).find(".btn-add-bookmark").click(function(){
+                app.add_bookmark(data,()=>{
+                    ce.msg("Add Bookmark success!","Bookmark","success");
+                });
+                return false;
+            });
+        }
         return emp_matche;
     }
 
@@ -130,6 +170,7 @@ class App{
         html+='</ul>';
 
         $('#body_main').html(html);
+        ce.top();
     }
 
     show_competitions(){
@@ -153,6 +194,7 @@ class App{
                 });
                 $("#list_competitions").append(emp_c);
             });
+            ce.top();
         },()=>{
             $('#body_main').html('<p>Error !</p>');
         });
@@ -165,22 +207,75 @@ class App{
         app.fetchData("scorers_"+id,"https://nodejs-server-fnc.vercel.app/api/goal_scorers.js?id="+id,(response)=>{
             Swal.close();
             var scorers=response.scorers;
-            $('#body_main').html('<ul class="list-group list-group-flush" id="list_competitions"></ul>');
-            $.each(scorers,function(index,m){
-                var player=m.player;
-                var team=m.team;
-                var html='';
-                html+='<li role="button" class="list-group-item">';
-                    html+='<i class="fas fa-user-ninja"></i> '+player.name+"<br/>";
-                    html+='<small class="text-muted">'+team.name+'</small> <small class="text-success" style="float:right"><i class="far fa-futbol"></i> '+m.goals+'</small>';
-                html+='</li>';
-                var emp_c=$(html);
-                $("#list_competitions").append(emp_c);
-            });
+            $("#body_main").empty();
+            if(scorers.length==0){
+                $("#body_main").html(app.none());
+            }else{
+                $('#body_main').html('<ul class="list-group list-group-flush" id="list_competitions"></ul>');
+                $.each(scorers,function(index,m){
+                    var player=m.player;
+                    var team=m.team;
+                    var html='';
+                    html+='<li role="button" class="list-group-item">';
+                        html+='<i class="fas fa-user-ninja"></i> '+player.name+"<br/>";
+                        html+='<small class="text-muted">'+team.name+'</small> <small class="text-success" style="float:right"><i class="far fa-futbol"></i> '+m.goals+'</small>';
+                    html+='</li>';
+                    var emp_c=$(html);
+                    $(emp_c).click(function(){
+                        app.show_player(m);
+                    });
+                    $("#list_competitions").append(emp_c);
+                });
+                ce.top();
+            }
         },()=>{
             Swal.close();
             $('#body_main').html('<p>Error !</p>');
         });
+    }
+
+    show_player(data){
+        var player=data.player;
+        var html='';
+        html+='<h5 class="text-center w-100">'+player.name+'</h5>';
+        if(data.playedMatches!=null) html+='<span class="btn btn-sm  btn-light m-1">Played Matches : <b>'+data.playedMatches+'</b></span>';
+        if(data.goals!=null) html+='<span class="btn btn-sm btn-light m-1">Goals : <b>'+data.goals+'</b></span>';
+        if(data.assists!=null) html+='<span class="btn btn-sm btn-light m-1">Assists : <b>'+data.assists+'</b></span>';
+        if(data.penalties!=null) html+='<span class="btn btn-sm btn-light m-1">Penalties : <b>'+data.penalties+'</b></span>';
+
+        html+='<ul class="list-group list-group-flush">';
+        if(data.player!=null){
+            html+='<li class="list-group-item">';
+                html+='<b>Info</b><br/>';
+                if(player.firstName!=null) html+='<small>First Name</small> : <span class="text-info">'+player.firstName+'</span><br/>';
+                if(player.lastName!=null) html+='<small>Last Name</small> : <span class="text-info">'+player.lastName+'</span><br/>';
+                if(player.dateOfBirth!=null) html+='<small>Date Of Birth</small> : <span class="text-info">'+player.dateOfBirth+'</span><br/>';
+                if(player.nationality!=null) html+='<small>Nationality</small> : <span class="text-info">'+player.nationality+'</span><br/>';
+                if(player.section!=null) html+='<small>Section</small> : <span class="text-info">'+player.section+'</span><br/>';
+                if(player.position!=null) html+='<small>Position</small> : <span class="text-info">'+player.position+'</span><br/>';
+                if(player.shirtNumber!=null) html+='<small>Shirt Number</small> : <span class="text-info">'+player.shirtNumber+'</span>';
+            html+='</li>';
+        }
+
+        if(data.team!=null){
+            var team=data.team
+            html+='<li class="list-group-item">';
+                html+='<b>Team</b><br/>';
+                if(team.crest) html+='<div class="w-100 text-center"><img style="width:30px" src="'+team.crest+'"></div>';
+                if(team.name!=null) html+='<small>Name</small> : <span class="text-info">'+team.name+'</span><br/>';
+                if(team.shortName!=null) html+='<small>Short Name</small> : <span class="text-info">'+team.shortName+'</span><br/>';
+                if(team.tla!=null) html+='<small>TLA</small> : <span class="text-info">'+team.tla+'</span><br/>';
+                if(team.address!=null) html+='<small>Address</small>:<a href="https://www.google.com/maps/search/'+team.address+'" class="text-info" target="_blank" rel="noopener noreferrer">'+team.address+'</a><br/>';
+                if(team.website!=null) html+='<small>Website</small>:<a href="'+team.website+'" class="text-info" target="_blank" rel="noopener noreferrer">'+team.website+'</a><br/>';
+                if(team.founded!=null) html+='<small>Founded</small> : <span class="text-info">'+team.founded+'</span><br/>';
+                if(team.clubColors!=null) html+='<small>Club Colors</small> : <span class="text-info">'+team.clubColors+'</span><br/>';
+                if(team.venue!=null) html+='<small>Venue</small> : <span class="text-info">'+team.venue+'</span><br/>';
+            html+='</li>';
+        }
+        html+='</ul>';
+
+        $('#body_main').html(html);
+        ce.top();
     }
 
     fetchData(id_data,url, callback,act_fail=null) {
@@ -191,7 +286,7 @@ class App{
             var currentTime = new Date().getTime();
             var elapsed = currentTime - timestamp;
 
-            if (elapsed < 180000) {
+            if (elapsed < 280000) {
                 callback(JSON.parse(data));
                 return;
             }
@@ -211,11 +306,55 @@ class App{
             }
         });
     }
+
+    show_bookmarks(){
+        app.menu_sel=2;
+        $("#body_main").html('<div class="row"><div class="col-12 pt-3 mb-3 text-center"><i class="fas fa-spinner fa-spin"></i></div></div>');
+
+        chrome.storage.local.get({bookmarks: []}, function (result) {
+            var list_bookmark = result.bookmarks;
+            $("#body_main").empty();
+            if(list_bookmark.length==0){
+                $("#body_main").html(app.none());
+            }
+            
+            $.each(list_bookmark,function(index,b){
+                b["index_offline"]=index;
+                $('#body_main').append(app.box_matche_item(b));
+            });
+        });
+
+        app.show_menu();
+        ce.top();
+    }
     
+    add_bookmark(data,act_done=null){
+        chrome.storage.local.get({bookmarks: []}, function (result) {
+            var list_bookmark = result.bookmarks;
+            data["index_offline"]=list_bookmark.length;
+            list_bookmark.push(data);
+            chrome.storage.local.set({bookmarks: list_bookmark}, function () {
+                if(act_done) act_done();
+            });
+        });
+    }
+
+    delete_item_bookmark(index,act_done=null){
+        chrome.storage.local.get({bookmarks: []}, function (result) {
+            var list_bookmark = result.bookmarks;
+            list_bookmark.splice(index, 1);
+            chrome.storage.local.set({bookmarks: list_bookmark}, function () {
+                if(act_done) act_done();
+            });
+        });
+    }
+
+    none(){
+        return '<div class="row"><div class="col-12 text-center mb-3 mt-3"><img src="images/none.gif"/><br/>List None</div></div>';
+    }
 }
 var app=new App();
 
 $(document).ready(function() {
     app.onLoad();
-    ce.get_list_app_other();
 });
